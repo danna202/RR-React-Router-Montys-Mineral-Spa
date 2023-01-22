2208abfd124cdf39f5644822ef2d1aaf39ec4d20
Danna Bohnhoff <dannab101@gmail.com>
2023-01-21 22:24:31 -0500

    initial commit

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,6 +1,9 @@
 
 
-export default function Packages() {
+export default function Packages({ packages }) {
+    const display = packages.map((elem, i) => {
+        return <li key={i}>{elem}</li>
+    })
 
     return (
         <div>
@@ -8,7 +11,7 @@ export default function Packages() {
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
                 </div>
-                
+                {display}
             </div>
         </div>
     )
